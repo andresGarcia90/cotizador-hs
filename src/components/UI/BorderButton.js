7c35a0f9531b1e66c05bff88efbdd7e7445a392f
andresGarcia90 <garcia.andi90@gmail.com>
2023-01-12 16:52:01 -0300

    Secondary button acction

--- src/components/UI/BorderButton.js
+++ src/components/UI/BorderButton.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import classes from './BorderButton.module.css';
 
-function BorderButton() {
+function BorderButton(props) {
+  
+  console.log(props);
+  const text = props.text && props.text.length > 0 ? props.text : '¿Qué me cubre?';
+
+
   return (
-    <button className={classes.button}>¿Qué me cubre?</button>
+    <button className={classes.button} onClick={props.clickHandler}>{text}</button>
   )
 }
 
--- src/components/UI/MainCard.js
+++ src/components/UI/MainCard.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { FiInfo } from "react-icons/fi";
+import { FiInfo } from 'react-icons/fi';
 
 // import cancelIcon from './../../img/cancel.svg';
 import PrimaryButton from './PrimaryButton';
@@ -8,6 +8,14 @@ import successIcon from './../../img/success.svg';
 import classes from './MainCard.module.css';
 import BorderButton from './BorderButton';
 const MainCard = (props) => {
+  const [borderText, setBorderText] = useState('¿Qué me cubre?');
+  const [showExtra, setShowExtra] = useState(false);
+  const borderClickHandler = (event) => {
+    setShowExtra(!showExtra);
+    setBorderText(!showExtra ? 'Mostrar menos' : '¿Qué me cubre?');
+    console.log(borderText);
+  };
+
   return (
     <Row>
       <Col className={classes['card-header']}>
@@ -47,56 +55,60 @@ const MainCard = (props) => {
         </Row>
       </Col>
 
-      <Col xs={12}>
-        <Row>
-          <Col xs={12} className={classes.hasta}>
-            hasta
-          </Col>
-          <Col xs={12}>
-            <Row>
-              <Col xs={8} className={classes['cobertura-item-title']}>
-              Robo/Hurto + Daños <FiInfo />
-              </Col>
-              <Col xs={4} className={classes['cobertura-item-price']}>
-              $4.000.000
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Col>
+      {showExtra && (
+        <Col xs={12}>
+          <Row>
+            <Col xs={12} className={classes.hasta}>
+              hasta
+            </Col>
+            <Col xs={12}>
+              <Row>
+                <Col xs={8} className={classes['cobertura-item-title']}>
+                  Robo/Hurto + Daños <FiInfo />
+                </Col>
+                <Col xs={4} className={classes['cobertura-item-price']}>
+                  $4.000.000
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      )}
 
-      <Col xs={12}>
-        <Row className={classes['asistencia-wrap']}>
-          <Col xs={12} className={classes['asistencia-title']}>
-            Asistencias
-          </Col>
-          <Col xs={12} className={classes.hasta}>
-            hasta
-          </Col>
-          <Col xs={12}>
-            <Row>
-              <Col xs={8} className={classes['asistencia-item-title']}>
-                ASISTENCIAS DEL HOGAR
-              </Col>
-              <Col xs={4} className={classes['asistencia-item-price']}>
-                $37.500
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={8} className={classes['asistencia-item-description']}>
-                <p>
-                  Servicios de urgencia de cerrajería, electricidad,
-                  cristalería, plomería y gas.
-                </p>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Col>
+      {showExtra && (
+        <Col xs={12}>
+          <Row className={classes['asistencia-wrap']}>
+            <Col xs={12} className={classes['asistencia-title']}>
+              Asistencias
+            </Col>
+            <Col xs={12} className={classes.hasta}>
+              hasta
+            </Col>
+            <Col xs={12}>
+              <Row>
+                <Col xs={8} className={classes['asistencia-item-title']}>
+                  ASISTENCIAS DEL HOGAR
+                </Col>
+                <Col xs={4} className={classes['asistencia-item-price']}>
+                  $37.500
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={8} className={classes['asistencia-item-description']}>
+                  <p>
+                    Servicios de urgencia de cerrajería, electricidad,
+                    cristalería, plomería y gas.
+                  </p>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      )}
       <Col xs={12}>
         <Row>
           <Col xs={12} className="my-2">
-            <BorderButton />
+            <BorderButton clickHandler={borderClickHandler} text={borderText} />
           </Col>
           <Col xs={12} className="my-2">
             <PrimaryButton />
